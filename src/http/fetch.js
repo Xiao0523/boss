5417c69112a24261c314da907b1868c4839cda18
lx <2239486223@qq.com>
2019-09-09 14:43:28 +0800

    "测试 自媒体每篇文章审核后 文章列表刷新"

--- src/http/fetch.js
+++ src/http/fetch.js
@@ -6,11 +6,11 @@ const server = axios.create({})
 server.defaults.withCredentials = true // 携带cookie
 
 if (process.env.NODE_ENV === 'development') {
-  server.defaults.baseURL = 'https://www.my51share.com/boss/' 
+  server.defaults.baseURL = 'https://dev.my51share.com/boss/' 
   // https://dev.my51share.com/boss/
   // https://www.my51share.com/boss/
 } else if (process.env.NODE_ENV === 'production') {
-  server.defaults.baseURL = 'https://www.my51share.com/boss/'
+  server.defaults.baseURL = 'https://dev.my51share.com/boss/'
 }
 // Add a request interceptor 添加请求拦截机
 server.interceptors.request.use(config => {
@@ -35,7 +35,7 @@ server.interceptors.response.use(res => { // 响应成功关闭loading
   const loadingInstance = Loading.service()
   setTimeout(() => {
     loadingInstance.close()
-  }, 1000)
+  }, 0)
   return res
 }, error => {
   const loadingInstance = Loading.service()
